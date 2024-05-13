2b901413b9db3ec36a0da0d86425ff3df75ed55a
Alexander Swan <43742263+Swan15@users.noreply.github.com>
2024-05-13 13:55:57 +1000

    Update action.ts

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,4 +1,5 @@
 'use server'
+
 import { emailMe } from '@emailme/emailme-js'
 
 export async function submitForm(
@@ -9,8 +10,19 @@ export async function submitForm(
         const email = formData.get('email')
         const details = formData.get('details')
         await emailMe(`contact from ${email}`, `Details: ${details}`)
+        // or you can just use a fetch request
+        // const res = await fetch('https://www.emailme.lol/api/send', {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         subject: `Contact from ${email}`,
+        //         description: details,
+        //         key: process.env.EMAIL_ME_API_KEY,
+        //     }),
+        // })
+        // console.log(await res.text())
         return [true, 'Email sent!']
     } catch (error) {
+        console.error(error)
         return [false, 'Error sending email!']
     }
 }
